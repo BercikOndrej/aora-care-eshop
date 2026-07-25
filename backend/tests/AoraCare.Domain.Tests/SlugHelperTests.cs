70644ed07a006bf24ee4f43a838d2a4f6c6e1589
Ondřej Berčík <ondrej.bercik@gmail.com>
2026-07-25 11:38:02 +0200

    test: add unit tests for SlugHelper

---
+++ backend/tests/AoraCare.Domain.Tests/SlugHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using AoraCare.Domain.Common;
+using FluentAssertions;
+
+namespace AoraCare.Domain.Tests;
+
+public class SlugHelperTests
+{
+    [Theory]
+    [InlineData("category 1", "category-1")]
+    [InlineData("Category Beauty", "category-beauty")]
+    [InlineData("-category Beauty-", "category-beauty")]
+    [InlineData("  category Beauty  ", "category-beauty")]
+    [InlineData("##category Beauty^~", "category-beauty")]
+    [InlineData("category $$#Beauty", "category-beauty")]
+    [InlineData("category_Beauty", "category-beauty")]
+    [InlineData("Příliš žluťoučký kůň", "prilis-zlutoucky-kun")]
+    [InlineData("a__b", "a-b")]
+    [InlineData("_category_", "category")]
+    public void CreateSlug_WhenIncludesSpaceAndNonWordChars_BeGoodFormatedSlug(
+        string name,
+        string expected
+    )
+    {
+        string slug = SlugHelper.CreateSlug(name);
+
+        slug.Should().Be(expected);
+    }
+}
